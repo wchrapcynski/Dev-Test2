84603e7a24a7488a487ba8493ced063f03525857
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-03 13:28:08 -0400

    Join form is now connected to a google sheet

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -8,10 +8,10 @@ function Signup() {
         <div className="signup-headline">Join Samantha!</div>
         <div className="signup-text">Sign up to receive the latest campaign updates.</div>
         <div className="signup-form">
-          <form className="forms-email">
-            <input className="email-address" type="email" name="email" placeholder="Email"/>
-            <input className="zipcode" type="text" name="zipcode" placeholder="ZIP"/>
-            <button className="submit">Join ></button>
+          <form className="forms-email" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfteSQKGDYkHA2g0ZneSFqGqTLxc4QONLtk_MCIiekvXi8cpg/formResponse" target="_self" method="POST">
+            <input className="email-address" type="email" name="entry.508402416" placeholder="Email" required />
+            <input className="zipcode" type="text" pattern="[0-9]{5}" title="Five digit zip code" size="5" name="entry.56774094" placeholder="ZIP" required />
+            <input className="submit" type="submit" value="Join >" />
           </form>
         </div>
       </div>
